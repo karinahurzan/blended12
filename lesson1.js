f00d3118ce7bd389f9144fb14ae9458c9b352314
RITAS <yuliya.lev.2000@gmail.com>
2025-01-11 13:11:28 +0200

    task3

--- lesson1.js
+++ lesson1.js
@@ -80,4 +80,26 @@ function chooseSeason(number) {
   } return result;  
 }
 
-console.log(chooseSeason(num));
+// console.log(chooseSeason(num));
+
+// Напишіть функцію getNumbers(min, max), що приймає 2 параметри - 
+// мінімальне і максимальне число відповідно.
+// Напишіть цикл всередині функції, який виводить у консоль
+// всі числа від max до min за спаданням.
+// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
+// і поверніть її з функції.
+
+function getNumbers(min, max) {
+let sum = 0;
+
+    for (let i = max; i >= min; i--){
+         console.log(i);
+        
+        if (i % 2 === 0) {
+        sum += i;
+        }
+    }
+    return sum;
+}
+console.log(getNumbers(5, 27));
+
