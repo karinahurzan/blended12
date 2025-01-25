d0b4543b065bf123b403c1b0b3b26218dfe9876a
karinahurzan <kgurzan@gmail.com>
2025-01-25 11:00:11 +0100

    task one

--- lesson2.js
+++ lesson2.js
@@ -1,1 +1,12 @@
+'use strict';
 
+//Напиши функцію findSmallerNumber(numbers)
+//яка шукає найменше число в масиві.
+//Додай перевірку що функція отримує масив
+const numbers = [12, 5, 35, 56, 3, 24, 7, 80, 12];
+
+function findSmallerNumber(numbers) {
+    return Math.min(...numbers);
+}
+
+console.log(findSmallerNumber(numbers));
