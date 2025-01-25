b91e76a9b11a9727a06058df7b111d38a79793ee
DmytroValMan <dmitri.m2011@gmail.com>
2025-01-25 11:30:24 +0200

    task two

--- lesson2.js
+++ lesson2.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 //Напиши функцію findSmallerNumber(numbers)
 //яка шукає найменше число в масиві.
@@ -6,7 +6,26 @@
 const numbers = [12, 5, 35, 56, 3, 24, 7, 80, 12];
 
 function findSmallerNumber(numbers) {
-    return Math.min(...numbers);
+  return Math.min(...numbers);
 }
 
 console.log(findSmallerNumber(numbers));
+
+//Напиши функцію findLongestWord(string)
+// //яка приймає довільний рядок
+// //що складається лише з розділених слів
+// //пробілом (параметр string)
+// //і повертає найдовше слово в цій строкі
+const string = "Nunc sed turpis a felis in nunc fringilla";
+
+function findLongestWord(string) {
+  const array = string.split(" ");
+  let longestWord = array[0];
+  for (const item of array) {
+    if (item.length > longestWord.length) {
+      longestWord = item;
+    }
+  }
+  return longestWord;
+}
+console.log(findLongestWord(string));
