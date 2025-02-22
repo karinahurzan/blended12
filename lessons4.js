ea057a089daaed3bb21b054384f8adb44d05c256
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-22 09:07:24 +0000

    2

--- lessons4.js
+++ lessons4.js
@@ -2,3 +2,19 @@ const input = document.querySelector("#alertInput");
 const button = document.querySelector("#alertButton");
 
 button.addEventListener("click", (event) => console.log(input.value));
+
+//  <!-- Завдання 2
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів. -->
+
+const btn = document.querySelector("#swapButton");
+const leftInput = document.querySelector("#leftSwapInput");
+const rightInput = document.querySelector("#rightSwapInput");
+
+btn.addEventListener("click", switchInput);
+
+function switchInput() {
+  const rightInputValue = leftInput.value;
+  leftInput.value = rightInput.value;
+  rightInput.value = rightInputValue;
+}
