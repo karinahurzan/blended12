f966ee59c5716b90aa8bed298d56c0530bbf6682
RITAS <yuliya.lev.2000@gmail.com>
2025-02-22 10:55:43 +0200

    task1

---
+++ lessons4.js
@@ -0,0 +1,4 @@
+const input = document.querySelector("#alertInput");
+const button = document.querySelector("#alertButton");
+
+button.addEventListener("click", (event) => console.log(input.value));
